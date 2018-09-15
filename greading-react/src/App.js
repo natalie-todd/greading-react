e932459d4db484501a9d04d0a486d3c8352203e3
Natalie <natalie.todd15@gmail.com>
2018-09-15 14:28:30 -0600

    changes to red at 2 and bold at 1

--- greading-react/src/App.js
+++ greading-react/src/App.js
@@ -14,20 +14,15 @@ class App extends Component {
   }
 
   nameChangedHandler = (event, id) => {
-// set variable personsIndex to the ids of state
     const personsIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
-//set variable person to personsIndices
     const person = {
       ...this.state.persons[personsIndex]
     };
-//name portion of state will equal event.target.value
     person.name = event.target.value;
-//persons will equal entire state object and the index of persons = person
     const persons = [...this.state.persons];
     persons[personsIndex] = person;
-//set the state so persons is the new object in state
     this.setState({ persons: persons })
   }
 
@@ -69,12 +64,20 @@ class App extends Component {
         </div>
       )
       style.backgroundColor = 'red';
-    };
+    }
+
+let classes = []; 
+if (this.state.persons.length <= 2) {
+  classes.push('red');
+}
+if(this.state.persons.length <= 1) {
+  classes.push('bold');
+}
 
   return (
       <div className = 'App' >
       <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
+      <p className={classes.join(' ')}>This is really working!</p>
         <button
           style = {style}
           onClick = { this.togglePersonsHandler } > Toggle Persons</button>
