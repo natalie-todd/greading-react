596a3ea0026aefcdd65db035c5780a66e5cc28b0
Natalie <natalie.todd15@gmail.com>
2018-09-15 14:49:59 -0600

    alternate method to Radium

--- greading-react/src/App.js
+++ greading-react/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -46,11 +45,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -69,10 +64,6 @@ class App extends Component {
         </div>
       )
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
     }
 
     let classes = [];
@@ -84,15 +75,15 @@ class App extends Component {
     }
 
     return (
-      <div className='App' >
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler} > Toggle Persons</button>
-        {persons}
-      </div>
+        <div className='App' >
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler} > Toggle Persons</button>
+          {persons}
+        </div>
     );
   }
 }
-export default Radium(App);
+export default App;
