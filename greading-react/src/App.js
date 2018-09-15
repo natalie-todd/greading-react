6df2b778799a5ea81bc3c2e4231be38c61a8192b
Natalie <natalie.todd15@gmail.com>
2018-09-15 14:38:23 -0600

    added Radium so we can use sudo selectors like hover inline. Button is now lighter when cursor hovers over it

--- greading-react/src/App.js
+++ greading-react/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
-class App extends Component { 
+class App extends Component {
   state = {
     persons: [
       { id: 'asdfa', name: 'Max', age: 28 },
@@ -10,7 +11,7 @@ class App extends Component {
       { id: 'tgted', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: false 
+    showPersons: false
   }
 
   nameChangedHandler = (event, id) => {
@@ -30,7 +31,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     //Splice is not best practice. It can lead to unpredictability.
     persons.splice(personsIndex, 1);
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -45,45 +46,53 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
 
-    if ( this.state.showPersons ) {
+    if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age} 
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
-let classes = []; 
-if (this.state.persons.length <= 2) {
-  classes.push('red');
-}
-if(this.state.persons.length <= 1) {
-  classes.push('bold');
-}
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
 
-  return (
-      <div className = 'App' >
-      <h1>Hi, I'm a React App</h1>
-      <p className={classes.join(' ')}>This is really working!</p>
+    return (
+      <div className='App' >
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
-          style = {style}
-          onClick = { this.togglePersonsHandler } > Toggle Persons</button>
-          {persons}
+          style={style}
+          onClick={this.togglePersonsHandler} > Toggle Persons</button>
+        {persons}
       </div>
     );
   }
 }
-export default App;
+export default Radium(App);
